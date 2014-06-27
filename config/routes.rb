21b7e633fea4583851159c9e7f5a8f9e640cc1a7
Daniel Kehoe <daniel@danielkehoe.com>
2014-06-26 18:56:30 -0700

    update for Rails 4.1.2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root :to => "visitors#index"
-  resources :users, :only => [:index, :show, :edit, :update ]
+  resources :users, :only => [:index, :show]
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
