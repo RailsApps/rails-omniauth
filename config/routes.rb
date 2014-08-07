a7badcfd3374f8e9b39841ad9b601fd4e9851222
Daniel Kehoe <daniel@danielkehoe.com>
2014-08-07 11:10:03 -0700

    remove the restrictions in the routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, :only => [:index, :show]
+  resources :users
   root to: 'visitors#index'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
