e817bc8e0431afc92b613d4501a7a4bee895c9bf
Gonzalo Goral <goral.gonzalo@gmail.com>
2018-02-23 18:43:49 -0300

    Update sessions_controller.rb
    Why use where message when find_by it's ok?

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,7 @@ class SessionsController < ApplicationController
 
   def create
     auth = request.env["omniauth.auth"]
-    user = User.where(:provider => auth['provider'],
-                      :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
+    user = User.find_by(provider: auth['provider'], uid: auth['uid']) || User.create_with_omniauth(auth)
     reset_session
     session[:user_id] = user.id
     redirect_to root_url, :notice => 'Signed in!'
